4809332031c6e5bc4688c5086110c56f49510d80
Abdallah Frehat <frehat.abdallah99@gmail.com>
2025-03-05 12:48:31 +0200

    updating code

--- webFundimantal/JS/Fizzbuzz/fizzbuzz.js
+++ webFundimantal/JS/Fizzbuzz/fizzbuzz.js
@@ -1,13 +1,15 @@
 function fizzbuzz() {
     for (var i = 1; i <= 100; i++) {
-        if (i % 3 == 0) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+        }
+        else if (i % 3 == 0) {
             console.log("Fizz");
         }
-        if (i % 5 == 0) {
+        else if (i % 5 == 0) {
             console.log("buzz");
-        }
-        if (i % 3 != 0 && i % 5 != 0) {
-            console.log(i);
+        }else{
+            console.log(i)
         }
     }
 }
