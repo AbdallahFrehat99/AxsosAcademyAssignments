157d8dd50c3961c001bdec48ae8dd535f2abcd74
Abdallah Frehat <frehat.abdallah99@gmail.com>
2025-03-11 16:25:04 +0200

    calculator

---
+++ webFundimantal/JS/Calculator/script.js
@@ -0,0 +1,51 @@
+var firstNum, secondNum, operation;
+
+function press(num) {
+    let displayVariable = document.querySelector("#display");
+    if (displayVariable.innerText == 0) {
+        displayVariable.innerText = num;
+    } else {
+        displayVariable.innerText += num;
+    }
+}
+function setOP(el) {
+    let displayVariable = document.querySelector("#display");
+    // console.log(el);
+    operation = el;
+    firstNum = parseInt(displayVariable.innerText);
+    displayVariable.innerText = 0;
+    // console.log(firstNum);
+}
+function clr() {
+    let displayVariable = document.querySelector("#display");
+    displayVariable.innerText = 0;
+    firstNum = null;
+    secondNum = null;
+    operation = null;
+}
+function calculate() {
+    let displayVariable = document.querySelector("#display");
+    secondNum = parseInt(displayVariable.innerText)
+    var calc;
+    // console.log(operation);
+    // console.log(secondNum);
+    switch (operation) {
+        case "+":
+            calc = firstNum + secondNum;
+            break;
+        case "-":
+            calc = firstNum - secondNum;
+            break;
+        case "*":
+            calc = firstNum * secondNum;
+            break;
+        case "/":
+            calc = firstNum / secondNum;
+            break;
+        default:
+            calc = "Invalid"
+    }
+    displayVariable.innerText = calc;
+    firstNum = calc;  
+    operation = null; 
+}
