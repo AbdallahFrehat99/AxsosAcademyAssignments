8d551603bd1a353c424d2d37c6243d6f679564e1
Abdallah Frehat <frehat.abdallah99@gmail.com>
2025-04-13 16:28:42 +0300

    great game

--- pythonStack/django/django_fundamental/Great_Number_Game/game_app/urls.py
+++ pythonStack/django/django_fundamental/Great_Number_Game/game_app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [    
     path('', views.index),  # Root path
+    path('guess',views.guess),
+    path('delete',views.delete),
+    path('again',views.again)
     ]
--- pythonStack/django/django_fundamental/Great_Number_Game/game_app/views.py
+++ pythonStack/django/django_fundamental/Great_Number_Game/game_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+import random
+def index(request):    
+    if 'randN' not in request.session:
+        request.session['randN']=random.randint(1, 100)
+    if 'res' not in request.session:
+        request.session['res']=''
 
-# Create your views here.
+    return render(request,'index.html')
+
+def guess(request):
+    if request.POST['entered-num']:
+        number=request.POST['entered-num']
+        if int(number) >request.session['randN']:
+            request.session['res']='high'
+            return redirect('/')
+        elif int(number) <request.session['randN']:
+            request.session['res']='low'
+            return redirect('/')
+        elif int(number) == request.session['randN']: 
+            request.session['res']='correct'
+            return redirect('/')
+    else:
+        return redirect('/')
+
+def delete(request):
+    request.session.clear()
+    return redirect('/')
+def again(request):
+    del request.session['randN']
+    del request.session['res']
+    return redirect('/')
