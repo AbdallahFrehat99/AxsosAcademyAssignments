e3a8e143c9f4f02b6a043565dc11c2040cee869a
Abdallah Frehat <frehat.abdallah99@gmail.com>
2025-04-15 22:28:15 +0300

    ninja gold

--- pythonStack/django/django_fundamental/NinjaGold/ninjagold/ninja/urls.py
+++ pythonStack/django/django_fundamental/NinjaGold/ninjagold/ninja/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [    
-    path('', views.index ,name ='index'),  # Root path
+    path('', views.index),  # Root path
+    path('earngold',views.earngold),
+    path('destroy',views.destroy)
     ]
--- pythonStack/django/django_fundamental/NinjaGold/ninjagold/ninja/views.py
+++ pythonStack/django/django_fundamental/NinjaGold/ninjagold/ninja/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+import random
 from django.http import HttpResponse
-def index(request):    
+from datetime import datetime
+def index(request):
+    if 'gold' not in request.session and 'activities' not in request.session:
+        request.session['gold']=0
+        request.session['activities']=[]
+        request.session['red']='red'
+        request.session['green']='green'
     return render(request,'index.html')
+def earngold(request):
+    now = datetime.now()
+    timestamp = now.strftime("(%B %d, %Y %#I:%M %p)")
+    if request.method=='POST' and request.POST['place'] == 'quest':
+        formPlace=request.POST['place']
+        gold_earned=random.randint(-50,50)
+        if gold_earned >= 0:
+            message=f"you entered a {formPlace} and earned {gold_earned} gold.{timestamp}"
+        else:
+            message=f"you entered a {formPlace} and lost {gold_earned} gold.{timestamp}"
+
+    elif request.method=='POST':
+        formPlace=request.POST['place']
+        gold_earned=random.randint(10,20)
+        message=f"you entered a {formPlace} and earned {gold_earned} gold.{timestamp}"
+
+    request.session['gold'] +=gold_earned
+    request.session['activities'].insert(0,message)
+        
+    return redirect('/')
+def destroy(request):
+    del request.session['gold']
+    del request.session['activities']
+    return redirect('/')
