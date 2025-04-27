11e46a990d31337a06151fd3c84195e73d824896
Abdallah Frehat <frehat.abdallah99@gmail.com>
2025-04-27 21:34:28 +0300

    flask counter

---
+++ pythonStack/Flask/counter/server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect,session
+app = Flask(__name__)  
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route('/')         
+def count():
+    if 'count' not in session:
+        session['count'] = 0
+    else:
+        session['count']+=1
+    return render_template("index.html")
+
+@app.route('/destroy_session')         
+def destroy_session():
+    session.clear()
+    return redirect('/')
+
+@app.route('/plus_two')         
+def plus_2():
+    if 'count' not in session:
+        session['count'] = 1
+    else:
+        session['count']+=1
+    return redirect('/')
+
+if __name__=="__main__":   
+    app.run(debug=True)    
