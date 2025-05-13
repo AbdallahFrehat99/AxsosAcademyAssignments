360a334a3b1c0bc41b5956fcdb00e71a3a0341e1
ABDALLAH S. FREHAT <frehat.abdallah99@gmail.com>
2025-05-13 13:40:11 +0300

    first java program

---
+++ JavaStack/JavaFundamental/FirstJavaProgram/First.java
@@ -0,0 +1,8 @@
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("my name is Abdallah Frehat");
+        System.out.println("I am 26 years old");
+        System.out.println("My hometown is Jenin, Palestine");
+    }
+}
