37f11b8eb5a952cd5e679687bfe3d77daf8a12ac
ABDALLAH S. FREHAT <frehat.abdallah99@gmail.com>
2025-05-22 15:52:13 +0300

    routes

---
+++ JavaStack/Spring/routes/src/main/java/com/example/routes/RoutesApplication.java
@@ -0,0 +1,31 @@
+package com.example.routes;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/daikichi")
+public class RoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutesApplication.class, args);
+	}
+
+	@RequestMapping("")
+    public String index() { 
+            return "Welcome!";
+    }
+	@RequestMapping("/today")
+    public String index1() { 
+            return "Today you will find luck in all your endeavors!";
+    }
+	@RequestMapping("/tomorrow")
+    public String index2() { 
+            return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+	
+}
