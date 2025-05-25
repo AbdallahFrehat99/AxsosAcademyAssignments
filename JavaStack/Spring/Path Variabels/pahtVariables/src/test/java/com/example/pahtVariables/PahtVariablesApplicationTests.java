659daa38d98f6c3243355cef5012cbdee906d36d
ABDALLAH S. FREHAT <frehat.abdallah99@gmail.com>
2025-05-25 15:38:32 +0300

    Paths

---
+++ JavaStack/Spring/Path Variabels/pahtVariables/src/test/java/com/example/pahtVariables/PahtVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pahtVariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahtVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
