822dd2ef8ada275b742afa05e7427be24d48a17a
ABDALLAH S. FREHAT <frehat.abdallah99@gmail.com>
2025-07-03 13:24:37 +0300

    putting it together

---
+++ MERN-Stack/React/putAllTogether/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HomeComponent from './components/HomeComponent'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <HomeComponent firstname = "Jane" lastname = "Doe" age = {45} haircolor = "Black"/>
+      <HomeComponent firstname = "John" lastname = "Smith" age = {88} haircolor = "Brown"/>
+    </>
+  )
+}
+
+export default App
