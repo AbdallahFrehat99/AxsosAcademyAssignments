4a53195ec1fc949413b118640528a3075dced234
ABDALLAH S. FREHAT <frehat.abdallah99@gmail.com>
2025-07-07 15:15:13 +0300

    axios pokemon

---
+++ MERN-Stack/React/Axios Pokemon/client/Axios-API/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { HomeComponent } from './components/HomeComponent'
+
+function App() {
+
+  return (
+    <>
+      <HomeComponent/>
+    </>
+  )
+}
+
+export default App
